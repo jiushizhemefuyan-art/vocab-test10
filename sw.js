676baa0ed26f90cb6b6bb8299131507a15e485a6
jiushizhemefuyan-art <jiushizhemefuyan@gmail.com>
2025-12-06 17:34:31 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('vocab-v1').then(c=>c.addAll(['./','./home.html','./quiz.html','./unit10.html','./styles.css','./data.js'])));});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
